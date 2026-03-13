0db7069266e537d8b96bf6e5f5b4bc91c9f72aba
duyp66cntt-collab <duy.p.66cntt@ntu.edu.vn>
2026-03-14 02:08:22 +0700

    Done

--- ViduArraylistsonguyen/src/listsonguyen/runmain.java
+++ ViduArraylistsonguyen/src/listsonguyen/runmain.java
@@ -9,18 +9,28 @@ public class runmain {
 		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
 		Scanner banPhim = new Scanner(System.in);
 		for(int i=0;i<10;i++) {
-		System.out.print("Moi nhap phan tu thu : " + i + ":");
+		System.out.print("Moi nhap phan tu thu " + i + ":");
 		Integer tam = banPhim.nextInt();
 		dsSoNguyen.add(tam);
 		}
+		System.out.print("Danh sach vua nhap la \n ");
 		for(int i=0; i<dsSoNguyen.size();i++) {
 			Integer tam = dsSoNguyen.get(i);
 			System.out.print(tam + " ");
 		}
+		int soluongPTchan=0;
+		for(int i=0; i<dsSoNguyen.size();i++) {
+			Integer tam = dsSoNguyen.get(i);
+			if( tam % 2 == 0) soluongPTchan=soluongPTchan+1;
 		
-		
-		
-
 	}
+		System.out.println(" \n So phan tu chan la: " + soluongPTchan);
+		int Tongall=0;
+		for(Integer x: dsSoNguyen) {
+			Tongall = Tongall +x;
+		}
+		System.out.println("tong tat ca So phan tu chan la: " + Tongall);
+		
 
+}
 }
