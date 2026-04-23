0efc8c1849a8537ab49bcb9932bfa8752db5f332
duyp66cntt-collab <duy.p.66cntt@ntu.edu.vn>
2026-04-23 08:12:14 +0700

    lần thứ 4 done câu 2

--- baithigiuaki_cau2/src/thigk2/phamduy/Cau2.java
+++ baithigiuaki_cau2/src/thigk2/phamduy/Cau2.java
@@ -1,9 +1,40 @@
 package thigk2.phamduy;
-
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
 public class Cau2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<SinhVien> ds = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		
+		// 1. add 3 sinh viên
+        ds.add(new SinhVien("66130695", "Phạm Duy", 2004, "CNTT"));
+        ds.add(new SinhVien("66130001", "Nguyễn Văn A", 2007, "Kế toán"));
+        ds.add(new SinhVien("66130002", "Trần Thị B", 2002, "Du lịch"));
+        
+        System.out.println("---Danh sách sinh viên ban đầu ---");
+        for (SinhVien sv : ds) System.out.println(sv);
+     // 2. Thêm mới 1 sinh viên từ bàn phím
+        System.out.println("\nNhập thông tin sinh viên mới:");
+        System.out.print("Mã SV: "); String ma = sc.nextLine();
+        System.out.print("Họ tên: "); String ten = sc.nextLine();
+        System.out.print("Năm sinh: "); int nam = sc.nextInt();
+        sc.nextLine(); // Đọc bỏ dòng thừa
+        System.out.print("Chuyên ngành: "); String nganh = sc.nextLine();
+        ds.add(new SinhVien(ma, ten, nam, nganh));
+        
+     // 3. In lại danh sách
+        System.out.println("\n--- Danh sách sau khi thêm ---");
+        for (SinhVien sv : ds) System.out.println(sv);
+     // 4. In sinh viên > 20 tuổi
+        int namHienTai = Calendar.getInstance().get(Calendar.YEAR);
+        System.out.println("\n--- Sinh viên trên 20 tuổi ---");
+		for(SinhVien sv : ds){
+			if((namHienTai - sv.getNamSinh()) > 20) {
+				System.out.println(sv);
+			}
+		}
 
 	}
 
--- baithigiuaki_cau2/src/thigk2/phamduy/SinhVien.java
+++ baithigiuaki_cau2/src/thigk2/phamduy/SinhVien.java
@@ -1,5 +1,27 @@
 package thigk2.phamduy;
 
 public class SinhVien {
-
+	private String maSV;
+	private String hoTen;
+	private int namSinh;
+	private String chuyenNganh; // Thuộc tính thứ 4 tự thêm
+	public SinhVien(String maSV, String hoTen, int namSinh, String chuyenNganh) {
+		
+		this.maSV = maSV;
+		this.hoTen = hoTen;
+		this.namSinh = namSinh;
+		this.chuyenNganh = chuyenNganh;
+	}
+	public String getMaSV() {return maSV;}
+	public void setMaSV(String maSV) { this.maSV = maSV; }
+    public String getHoTen() { return hoTen; }
+    public int getNamSinh() { return namSinh; }
+	@Override
+	public String toString() {
+		return "SinhVien [maSV=" + maSV + ", hoTen=" + hoTen + ", namSinh=" + namSinh + ", chuyenNganh=" + chuyenNganh
+				+ "]";
+	}
+	
+	
+	
 }
