f0488747e0ca0cc5c26b6cbab6268e6a2b390c50
duyp66cntt-collab <duy.p.66cntt@ntu.edu.vn>
2026-04-23 08:32:21 +0700

    Lần thứ 6 câu 3 done

--- baithigiuaki_cau3/src/thigk2/phamduy/Cau3.java
+++ baithigiuaki_cau3/src/thigk2/phamduy/Cau3.java
@@ -1,9 +1,38 @@
 package thigk2.phamduy;
-
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Cau3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Integer> danhSachSo = new ArrayList<>();
+		int X = 5;
+		boolean timThay = false;
+		
+		try {
+			File myFile = new File("C:\\Users\\206NDN-H21\\Documents\\GitHub\\66130695-JavaProgramming\\baithigiuaki_cau3\\src\\thigk2\\phamduy\\data.txt");
+			Scanner reader = new Scanner(myFile);
+			
+			while(reader.hasNextInt()) {
+				int so = reader.nextInt();
+				danhSachSo.add(so);
+				if(so == X) timThay = true;
+			}
+			reader.close();
+			
+			System.out.println("Danh sách số nguyên từ file: ");
+			System.out.println(danhSachSo);
+			
+			if(timThay) {
+				System.out.println("=> Tìm thấy giá trị X (\" + X + \") trong danh sách.\"");
+	
+			}else {
+				System.out.println("=> Không tìm thấy giá trị X (" + X + ") trong danh sách.");
+			}
+		}catch(Exception e) {
+			System.out.println("Lỗi: Không tìm thấy file hoặc định dạng file sai.");
+		}
+		
 
 	}
 
